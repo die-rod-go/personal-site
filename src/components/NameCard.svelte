49eaefbe4f34d4d4064b38070465a0a03c80b52f
die-rod-go <diegorod7724@gmail.com>
2024-07-11 17:35:11 -0400

    moved name card to component

---
+++ src/components/NameCard.svelte
@@ -0,0 +1,11 @@
+<div class="relative max-w-fit ml-16 mt-36">
+  <div class="bg-cus-primary p-16 w-fit max-w-120 relative z-20">
+    <p class="text-cus-secondary font-mono font text-2xl lg:text-9xl">
+      Hi, I'm Diego.
+    </p>
+  </div>
+  <div class="bg-cus-secondary absolute top-5 left-5 p-16 h-full w-full"></div>
+</div>
+<p class="font-mono ml-16 mt-10 text-cus-secondary text-1xl lg:text-3xl">
+  A software developer from Pennsylvania
+</p>
