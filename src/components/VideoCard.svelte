c2c3ae10adb514f72487f1aae722577e42a448d7
die-rod-go <diegorod7724@gmail.com>
2024-07-16 10:57:47 -0400

    muted videos

--- src/components/VideoCard.svelte
+++ src/components/VideoCard.svelte
@@ -1,16 +1,14 @@
 <script>
-    export let videoSrc = "";
-    export let tailwind = "";
+  export let videoSrc = "";
+  export let tailwind = "";
 </script>
 
 <div class="relative max-w-fit {tailwind}">
-    <div class="bg-cus-primary p-2 w-fit relative z-10 right-0">
-        <video class="h-96 z-10" autoplay loop>
-            <source src={videoSrc} type="video/mp4" />
-            <track kind="captions" />
-        </video>
-    </div>
-    <div
-        class="bg-cus-secondary absolute top-5 left-5 p-16 h-full w-full"
-    ></div>
+  <div class="bg-cus-primary p-2 w-fit relative z-10 right-0">
+    <video class="h-96 z-10" autoplay loop muted>
+      <source src={videoSrc} type="video/mp4" />
+      <track kind="captions" />
+    </video>
+  </div>
+  <div class="bg-cus-secondary absolute top-5 left-5 p-16 h-full w-full"></div>
 </div>
